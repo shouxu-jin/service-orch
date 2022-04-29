72e92362493c2563ebf74e2ef4f6166cea746cd7
red哞 <oncwnuOLwJqJkpqlHkmGKYn9PClA@git.weixin.qq.com>
2022-04-29 15:37:12 +0800

    添加测试

---
+++ service-server/src/test/java/com/nari/serviceorch/serviceserver/ServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nari.serviceorch.serviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
