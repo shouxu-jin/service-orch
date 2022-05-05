85905312cf79d0a8e3d5f740cf6184c19831b1b2
red哞 <oncwnuOLwJqJkpqlHkmGKYn9PClA@git.weixin.qq.com>
2022-05-05 10:24:35 +0800

    啊!!!

--- service-server/src/main/java/com/nari/service/orch/serviceserver/engine/executors/HttpNodeExecutor.java
+++ service-server/src/main/java/com/nari/service/orch/serviceserver/engine/executors/HttpNodeExecutor.java
@@ -3,20 +3,23 @@ package com.nari.service.orch.serviceserver.engine.executors;
 import com.nari.service.orch.define.Node;
 import com.nari.service.orch.define.nodes.HttpNode;
 import com.nari.service.orch.serviceserver.util.FreemarkerUtil;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Service
-public class HttpNodeExecutor implements NodeExecutor {
+public class HttpNodeExecutor implements NodeExecutor, InitializingBean {
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
 
     @Override
     public boolean support(Node node) {
@@ -43,5 +46,11 @@ public class HttpNodeExecutor implements NodeExecutor {
     @Override
     public Node chooseNext(Node current) {
         return current.getLineNodes().get(0).getNode();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
     }
 }
